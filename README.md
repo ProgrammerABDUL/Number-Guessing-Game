7a4206f8aab71075b970ddf3fd57ba6081c737be
ProgrammerABDUL <abdulrehmanjiwani5@gmail.com>
2025-03-26 13:46:52 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# 🎮 Number Guessing Game
+
+## 📝 Overview
+The Number Guessing Game is a simple CLI-based game where the player tries to guess a randomly generated number within a specified range. The game provides feedback on whether the guessed number is too high, too low, or correct.
+
+## ⭐ Features
+- 🎲 Random number generation within a specified range
+- ⌨️ User input for guessing the number
+- 💭 Feedback on each guess (too high, too low, correct)
+- 🔢 Count of the number of attempts taken to guess the correct number
+
+## 🔮 Upcoming Features
+- 🏆 High Scores: Track and display the highest scores achieved by players
+- 🎮 Game Mode: Introduce different game modes for varied difficulty levels
+
+## 🎯 How to Play
+1. Run the game from the command line
+2. The game will prompt you to guess a number within a specified range
+3. Enter your guess and press Enter
+4. The game will provide feedback on whether your guess is too high, too low, or correct
+5. Continue guessing until you find the correct number
+6. The game will display the number of attempts taken to guess the correct number
+
+## 🔧 Requirements
+- Python 3.x
+
+## 📥 Installation
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/ProgrammerABDUL/Number-Guessing-Game.git
+    ```
+2. Navigate to the project directory:
+    ```sh
+    cd Number-Guessing-Game
+    ```
+
+## 🚀 Running the Game
+To start the game, run the following command:
+```sh
+python main.py
+```
+
+## 📄 License
+This project is licensed under the MIT License.
+
+## 🤝🏻 Contributing
+Contributions are welcome! Please fork the repository and submit a pull request.
+
+## 📬 Contact
+For any questions or feedback, please reach out to me on [LinkedIn](https://pk.linkedin.com/in/abdul-rehman-jiwani).
---
+++ main.py
@@ -0,0 +1,82 @@
+import random
+
+# Title
+game_title = "\033[1m🎮Number Guessing Game🎲\033[0m"
+print(game_title)
+
+# Intro
+game_introduction = "A game, where you'll get few attempts to guess a number between 0 to 100. Are you ready? Let's get started! 🚀\n"
+print(game_introduction)
+
+# How to Play
+how_to_play = """\n\033[1m📖 How to Play: -\033[0m 
+1. \033[1m🎯 Guess the number:\033[0m The game will ask you to guess a number between 0 and 100.
+2. \033[1m💡 Get hints:\033[0m After each guess, the game will tell you if your guess was too high or too low.
+3. \033[1m⏳ Limited attempts:\033[0m You have a total of 5 attempts to guess the correct number.
+4. \033[1m🏆 Win or Lose:\033[0m If you guess the number within 5 attempts, you win! Otherwise, you lose, and the game will reveal the secret number."""
+
+# Options
+options = """\n\033[1m🎮 Options: -\033[0m
+1. 🎲 Start Game
+2. 📖 How to Play
+3. 🏆 View High Scores
+4. 🚪 Exit
+"""
+print(options)
+
+# Game Logic
+def Game():
+    random_number = random.randint(1, 101)
+    # print(random_number)
+
+    attempts = 5
+
+    print("\n\033[1m\033[3m🎮 Game has started!\033[0m\033[0m\n")
+
+    guess = int(input("Guess the number: "))
+
+    while guess != random_number and attempts > 0:
+        
+        if guess > random_number:
+            print(f"📈 Too High! Try Again, You've {attempts} attempt left.")
+
+        elif guess < random_number:
+            print(f"📉 Too Low! Try Again, You've {attempts} attempt left.")
+
+        guess = int(input("\nGuess the number: "))
+        attempts -= 1
+    
+    if guess == random_number:
+        print(f"\n🎉 Congrats! You won 🏆, You guessed \033[1m{guess}\033[0m correctly in \033[1m{attempts}\033[0m attempts!")
+        print("Thanks for playing! Goodbye 👋")
+
+    elif guess != random_number:
+        print(f"😢 You lost!, The number was {random_number}")
+        chance = input("🔄 Wanna try Again? (Y/N): ")
+
+        if chance == "Y" or chance == "y":
+            Game()
+        elif chance == "N" or chance == "n":
+            print("Thanks for playing! Goodbye 👋")
+        else:
+            print("Invalid Input")
+
+
+# Navigation
+navigation = int(input("Navigate: "))
+
+while navigation != "4":
+    if navigation == 1:
+        Game()
+        break
+    elif navigation == 2:
+        print(how_to_play)
+    elif navigation == 3:
+        print("\nHigh Score feature will be added soon.")
+    elif navigation == 4:
+        exit()
+    else:
+        print("Invalid Input")
+    
+    print(options)
+    navigation = int(input("Navigate: "))
